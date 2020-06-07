ab7bbcd09b1881335fb7359787320b5d0867d390
Patrick Oliver <poliver24@hotmail.com>
2020-06-07 18:14:27 +0100

    removed uneccessary comments

--- App.js
+++ App.js
@@ -1,7 +1,5 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-// import * as React from 'react';
-// import { Platform, StatusBar, StyleSheet, View } from 'react-native';
 
 import useCachedResources from './hooks/useCachedResources';
 import BottomTabNavigator from './navigation/BottomTabNavigator';
@@ -44,25 +42,9 @@ function App(props) {
     );
   }
 }
-//     );
-//   }
-// }
+
 
 export default withAuthenticator(App, { includeGreetings: true })
-// class App extends React.Component {
-//   render() {
-//     return ( 
-//     <View style={styles.container}>
-//         {Platform.OS === 'ios' && <StatusBar barStyle="dark-content" />}
-//         <NavigationContainer linking={LinkingConfiguration}>
-//           <Stack.Navigator>
-//             <Stack.Screen name="Root" component={BottomTabNavigator} />
-//           </Stack.Navigator>
-//         </NavigationContainer>
-//       </View>
-//     );
-//   }
-// }
 
 
 const styles = StyleSheet.create({
