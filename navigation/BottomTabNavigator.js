0516b13a662001ab6fe01936e1f1d77cefef885d
DavidStewartLDN <davidstewart.work@gmail.com>
2020-06-19 17:10:06 +0100

    added Chat page to bottom nav and files created

--- navigation/BottomTabNavigator.js
+++ navigation/BottomTabNavigator.js
@@ -7,6 +7,7 @@ import HomeScreen from '../screens/HomeScreen';
 import LinksScreen from '../screens/LinksScreen';
 import DiaryScreen from '../screens/DiaryScreen';
 import ScoreScreen from '../screens/ScoreScreen';
+import ChatScreen from '../screens/ChatScreen';
 
 const BottomTab = createBottomTabNavigator();
 const INITIAL_ROUTE_NAME = 'Home';
@@ -25,6 +26,16 @@ export default function BottomTabNavigator({ navigation, route }) {
         options={{
           title: 'Get Started',
           tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="md-code-working" />,
+        }}
+      />
+      <BottomTab.Screen
+        name="Chat"
+        component={ChatScreen}
+        options={{
+          title: 'Chat',
+          tabBarIcon: ({ focused }) => (
+            <TabBarIcon focused={focused} name="ios-chatboxes" />
+          ),
         }}
       />
       <BottomTab.Screen
